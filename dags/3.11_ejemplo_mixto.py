1f0be81b25aa170fd259ae8bebacb5bea6ec3ac1
Ian Marco Antonio Rumiche Juarez <ianrj19@gmail.com>
2024-04-15 21:59:43 -0500

    Cambios

--- dags/3.11_ejemplo_mixto.py
+++ dags/3.11_ejemplo_mixto.py
@@ -44,13 +44,12 @@ def filtrar_columnas(ti, valor): # TASK_ID='filtrar'
 def cargar_datos_mysql(ti,tabla_destino):
     archivo_subido = ti.xcom_pull(key='final', task_ids='TRANSFORMACION.filtrar')
 
-    engine = create_engine("mysql+pymysql://root:dwh@localhost:3306/dwh")
+    engine = create_engine("mysql+pymysql://root:dwh@mysql_dwh:3306/dwh")
     connection = engine.connect()
 
     with engine.begin() as connection:
         #base = pd.read_sql_query(f"SELECT * FROM Base_Consolidada", con=connection)
         archivo_subido.to_sql(name=tabla_destino, con=connection, if_exists='append', index=True)
-
 
 
 # Definir el DAG principal
