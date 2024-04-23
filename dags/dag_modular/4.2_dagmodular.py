c1b867d4ad513c3adb6a59bc5c2d9a1b55051edc
Ian Marco Antonio Rumiche Juarez <ianrj19@gmail.com>
2024-04-22 21:26:38 -0500

    Cambios

--- dags/dag_modular/4.2_dagmodular.py
+++ dags/dag_modular/4.2_dagmodular.py
@@ -8,15 +8,12 @@ from airflow.providers.mysql.hooks.mysql import MySqlHook
 from airflow.sensors.filesystem import FileSensor
 from airflow.operators.bash import BashOperator
 from datetime import datetime,timedelta
-import logging
 from sqlalchemy import create_engine
-import os
 
 
 # IMPORTANDO FUNCIONES GENERALES
-from dags.dag_modular.general.general_funciones import yaml_to_dict
+#from dags.dag_modular.general.general_funciones import get_sql
 from dags.dag_modular.general.general_funciones import get_config
-from dags.dag_modular.general.general_funciones import get_sql
 from dags.dag_modular.general.general_funciones import get_md
 
 # IMPORTANDO FUNCIONES
@@ -25,47 +22,52 @@ from dags.dag_modular.funciones.filtrar_columnas import filtrar_columnas
 from dags.dag_modular.funciones.leer_archivos import leer_archivos
 from dags.dag_modular.funciones.seleccion_columnas import seleccion_columnas
 
-
 # IMPORTANDO CONFIGURACIONES
-config_cargar = get_config(dag_file_name = dag_modular, config_filename = 'config_cargar.yaml')
-config_extraer = get_config(dag_file_name = dag_modular, config_filename = 'config_extraer.yaml')
-config_filtrar = get_config(dag_file_name = dag_modular, config_filename = 'config_filtrar.yaml')
-config_final = get_config(dag_file_name = dag_modular, config_filename = 'config_final.yaml')
-config_inicio = get_config(dag_file_name = dag_modular, config_filename = 'config_inicio.yaml')
-config_seleccion = get_config(dag_file_name = dag_modular, config_filename = 'config_seleccion.yaml')
-general_config = get_config(dag_file_name = dag_modular, config_filename = 'general_config.yaml')
+config_cargar = get_config('config_cargar.yaml')
+config_extraer = get_config('config_extraer.yaml')
+config_filtrar = get_config('config_filtrar.yaml')
+config_final = get_config('config_final.yaml')
+config_inicio = get_config('config_inicio.yaml')
+config_seleccion = get_config('config_seleccion.yaml')
+general_config = get_config( 'general_config.yaml')
 
+# IMPORTANDO CARACTERISTICAS DE EJECUCIÓN
+default_args = get_config( 'general_config.yaml')['default_args']['dev']
+dag_caracteristicas= get_config( 'general_config.yaml')['dag_caracteristicas']['dev']
 
-# Declare configuration variables
-dag_file_name = os.path.basename(__file__).split('.')[0]
-env=os.getenv('ENVIRONMENT')
-default_arguments = dag_config['default_args'][env]
-
-# Getting variables of pipeline configs
-endpoint = pipeline_config['endpoint']
-soccer_teams_ids = pipeline_config['soccer_teams_ids']
-
-#Airflow docstring
-doc_md = get_md(dag_file_name, 'README.md')
+# IMPORTANDO MARKDOWN
+doc_md = get_md( 'README.md')
 
 
 # Definir el DAG principal
 with DAG(
     dag_id='dag_modular',
-    default_args=general_config,
+    default_args=default_args,
     tags=["MODULO_4"],
+    schedule_interval= dag_caracteristicas['schedule_interval'],
+    start_date= dag_caracteristicas['start_date'],
+    max_active_runs= dag_caracteristicas['max_active_runs'],
+    catchup=dag_caracteristicas['catchup'],
+    max_active_tasks= dag_caracteristicas['max_active_tasks']   ,
+    description= dag_caracteristicas['description']
+
 ) as dag:
 
 
     INICIO = FileSensor(
         task_id='INICIO',
         fs_conn_id='my_filesystem',
+        filepath=config_inicio['filepath'],
+        poke_interval=config_inicio['poke_interval'],
+        timeout=config_inicio['timeout']
     )
 
     EXTRAER = PythonOperator( 
         task_id='EXTRAER',
         python_callable=leer_archivos,
         provide_context=True,
+        op_kwargs=config_extraer['op_kwargs']
+
     )
 
     with TaskGroup(group_id='TRANSFORMACION') as TRANSFORMACION:
@@ -74,12 +76,14 @@ with DAG(
             task_id='seleccion',
             python_callable=seleccion_columnas,
             provide_context=True,
+            op_kwargs=config_seleccion['op_kwargs']
         )
 
         filtrar = PythonOperator(
             task_id='filtrar',
             python_callable=filtrar_columnas,
             provide_context=True,
+            op_kwargs=config_seleccion['valor']
             
         )
         seleccion>> filtrar 
@@ -89,11 +93,14 @@ with DAG(
         task_id='CARGAR',
         python_callable=cargar_datos_mysql,
         provide_context=True,
+        op_kwargs=config_cargar['op_kwargs']
         
     )
 
     FINAL = BashOperator(
         task_id='FINAL',
+        bash_command=config_final['bash_command'],
+        trigger_rule=config_final['trigger_rule']
     )
 
     INICIO >> EXTRAER >> TRANSFORMACION >> CARGAR >> FINAL
--- dags/dag_modular/general/general_funciones.py
+++ dags/dag_modular/general/general_funciones.py
@@ -1,47 +1,58 @@
 import os
 import yaml
-from airflow import DAG
 from os import path
 
-def yaml_to_dict(dag_file_name=None, config_filename=None):
+
+def yaml_to_dict(config_filename):
     """
-    Parses yaml file into dict
-    :param file_name: Name of file
-    :return: dict
+    Parses yaml file into dict from the configurations directory.
+    
+    :param config_filename: Name of the YAML configuration file.
+    :return: dict loaded from the YAML file.
     """
+    # Construye la ruta al archivo YAML ubicado en el subdirectorio 'configuraciones'.
     dir_name = path.dirname(__file__)
-    path_to_file = path.join(dir_name, "../../dags/", f"{dag_file_name}/{config_filename}")
-    with open(path_to_file) as file:
+    path_to_file = path.join(dir_name, 'configuraciones', config_filename)
+
+    with open(path_to_file, 'r') as file:
         config = yaml.safe_load(file)
     return config
 
-def get_config(dag_file_name, config_filename):
+def get_config(config_filename):
     """
-    Creates final config dict out of global and dag specific yamls
-    :return: dict
-    """
-    dag_dict = yaml_to_dict(dag_file_name,config_filename)
-
-    return dag_dict
-
-def get_sql(dag_file_name, filename):
-    """
-    Gets sql file into string
-    :param filename: Name of file
-    :return: string with sql code
-    """
-    dir_name = os.path.dirname(__file__)
-    path_to_file = os.path.join(dir_name, "../../dags/", f"{dag_file_name}/sql/{filename}")
-    with open(path_to_file, 'r') as file:
-        return file.read()   
+    Retrieves the configuration dictionary from a YAML file in the configurations directory.
     
-def get_md(dag_file_name, filename):
+    :param config_filename: Name of the YAML configuration file to load.
+    :return: Configuration dictionary.
     """
-    Gets markdown file into string
-    :param filename: Name of file
-    :return: string with markdown code
+    return yaml_to_dict(config_filename)
+
+
+
+def get_sql(filename):
     """
+    Lee un archivo SQL desde el subdirectorio 'sql' y devuelve su contenido como una cadena de texto.
+    
+    :param filename: Nombre del archivo SQL.
+    :return: Contenido del archivo SQL como cadena de texto.
+    """
+    # Construye la ruta al archivo SQL ubicado en el subdirectorio 'sql'.
     dir_name = os.path.dirname(__file__)
-    path_to_file = os.path.join(dir_name, "../../dags/", f"{dag_file_name}/{filename}")
+    path_to_file = os.path.join(dir_name, 'sql', filename)
+
+    with open(path_to_file, 'r') as file:
+        return file.read()
+
+def get_md(filename):
+    """
+    Lee un archivo Markdown desde el directorio principal del DAG y devuelve su contenido como una cadena de texto.
+    
+    :param filename: Nombre del archivo Markdown.
+    :return: Contenido del archivo Markdown como cadena de texto.
+    """
+    # Construye la ruta al archivo Markdown ubicado en el directorio actual.
+    dir_name = os.path.dirname(__file__)
+    path_to_file = os.path.join(dir_name, filename)
+
     with open(path_to_file, 'r') as file:
         return file.read()
