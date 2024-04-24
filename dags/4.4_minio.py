bffc75e0cefddb27058c1b2d338a80cb21814d16
Ian Marco Antonio Rumiche Juarez <ianrj19@gmail.com>
2024-04-24 15:04:11 -0500

    Cambios

---
+++ dags/4.4_minio.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.providers.amazon.aws.transfers.local_to_s3 import LocalFilesystemToS3Operator
+from airflow.providers.amazon.aws.transfers.s3_to_local import S3ToLocalFilesystemOperator
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+
+def get_s3_hook():
+    return S3Hook(aws_conn_id='my_minio_conn', region_name='us-east-1')
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 1, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+}
+
+dag = DAG(
+    'minio_file_transfer',
+    default_args=default_args,
+    description='A simple DAG to transfer files to and from MinIO',
+    schedule_interval='@daily',
+)
+
+upload_to_minio = LocalFilesystemToS3Operator(
+    task_id='upload_to_minio',
+    filename='/path/to/local/file',
+    dest_key='s3_key',
+    dest_bucket='my_bucket',
+    replace=True,
+    aws_conn_id='my_minio_conn',
+    dag=dag,
+)
+
+download_from_minio = S3ToLocalFilesystemOperator(
+    task_id='download_from_minio',
+    s3_key='s3_key',
+    local_path='/path/to/destination/file',
+    bucket_name='my_bucket',
+    aws_conn_id='my_minio_conn',
+    dag=dag,
+)
+
+upload_to_minio >> download_from_minio
---
+++ dags/4.5_snowflake.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 1, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+}
+
+dag = DAG(
+    'snowflake_operations',
+    default_args=default_args,
+    description='A DAG to interact with Snowflake',
+    schedule_interval='@daily',
+)
+
+sql_query = """
+SELECT * FROM my_table LIMIT 10;
+"""
+
+run_query = SnowflakeOperator(
+    task_id='run_query',
+    sql=sql_query,
+    snowflake_conn_id='my_snowflake_conn',
+    dag=dag,
+)
+
+run_query
